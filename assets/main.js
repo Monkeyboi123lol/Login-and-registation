b3c86fede315fc72ed36dcf6dbda8636dc3f8a34
Monkeyboi123lol <fawoleatilolamoses@gmail.com>
2026-02-03 17:35:06 -0500

    update

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+function signupNavigation() {
+  let loginForm = document.querySelector(".box");
+  let registrationForm = document.querySelector(".registration-form");
+  loginForm.style.display = "none";
+  registrationForm.style.display = "block";
+}
+
+function Navigation() {
+  let registrationForm = document.querySelector(".registration-form");
+  let loginForm = document.querySelector(".box");
+  registrationForm.style.display = "none";
+  loginForm.style.display = "block";
+}
