8813faa9c48b03d4f0045a10ab2b99933cb46f48
Monkeyboi123lol <fawoleatilolamoses@gmail.com>
2026-02-16 17:41:24 -0500

    Update1.0

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,18 @@
+let registration_name;
+let registration_email;
+let registration_password;
+let registration_confirm_password;
+
 function registration() {
-  let registration_name = document.querySelector("#registration-name").value;
-  let registration_email = document.querySelector("#registration-email").value;
-  let registration_password = document.querySelector(
+  registration_name = document.querySelector("#registration-name").value;
+  registration_email = document.querySelector("#registration-email").value;
+  registration_password = document.querySelector(
     "#registration-password",
   ).value;
-  let registration_confirm_password = document.querySelector(
+  registration_confirm_password = document.querySelector(
     "#registration-confirm-password",
   ).value;
-  let registration_error = document.querySelector(".registration-error");
+  registration_error = document.querySelector(".registration-error");
 
   if (registration_name == "") {
     registration_error.innerHTML = "Please enter your name.";
@@ -49,4 +54,29 @@ function Navigation() {
   let loginForm = document.querySelector(".box");
   registrationForm.style.display = "none";
   loginForm.style.display = "block";
+}
+
+function login() {
+  let login_email = document.querySelector("#login-email").value;
+  let login_password = document.querySelector("#login-password").value;
+  let login_error = document.querySelector(".login-error");
+
+  if (login_email == "") {
+    login_error.innerHTML = "Please enter your email.";
+  } else if (login_password == "") {
+    login_error.innerHTML = "Please enter your password.";
+  } else if (
+    login_email == registration_email &&
+    login_password == registration_password
+  ) {
+    console.log("Login successful!");
+    alert("Login successful! Welcome, " + registration_name + "!");
+    document.querySelector("#login-email").value = "";
+    document.querySelector("#login-password").value = "";
+    window.location.href = "dashboard.html";
+  } else {
+    login_error.innerHTML =
+      "Invalid email or password. Please Register if you don't have an account.";
+  }
+  event.preventDefault();
 }
